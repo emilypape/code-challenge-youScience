0f59912e2cd7c7bdfdceb59f9df8c864b74b4543
emily <eap6787@gmail.com>
2022-11-18 18:14:13 -0700

    create a route that finds and returns all of the data for a specific item

--- server/database/controllers/apiRoutes.js
+++ server/database/controllers/apiRoutes.js
@@ -2,13 +2,27 @@ const router = require('express').Router();
 const sequelize = require('../connection');
 const { List, Item } = require('../models');
 
-//
+//route that finds a list and returns all of its items with their name and status
 router.get('/list', async (req, res) => {
   List.findAll({
     attributes: ['list_name'],
     include: {
       model: Item,
       attributes: ['item_name', 'status'],
+    },
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// finds a specific item and returns all fields for that item
+router.get('/item/:id', (req, res) => {
+  Item.findOne({
+    where: {
+      id: req.params.id,
     },
   })
     .then((data) => res.json(data))
