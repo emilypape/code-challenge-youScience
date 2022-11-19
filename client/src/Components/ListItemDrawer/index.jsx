9bdcc843fdbeec664c42b9814b06412515d85112
emily <eap6787@gmail.com>
2022-11-19 15:26:13 -0700

    add a save button to the drawer component

--- client/src/Components/ListItemDrawer/index.jsx
+++ client/src/Components/ListItemDrawer/index.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import '../../assets/styles/index.css';
 import { Checkbox, Drawer, FormControlLabel, FormLabel, Input, Radio, RadioGroup, FormControl } from '@mui/material';
-import { blueGrey } from '@mui/material/colors';
+import { blueGrey, green } from '@mui/material/colors';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { bgcolor, color, margin } from '@mui/system';
@@ -17,15 +17,18 @@ export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
     description: '',
     status: false,
   };
-
+  // create a state variable to hold the status value
   const [value, setValue] = React.useState('Controlled');
 
+  // when the radio has an event change the state to the value of that radio button
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
     <Drawer
+      // change drawer styles
+      // set on close to change the state of the drawer
       PaperProps={{
         sx: { width: '50%', bgcolor: blueGrey[900], color: '#FFFFFF' },
       }}
@@ -34,10 +37,12 @@ export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
       anchor='right'
       // variant='persistent'
     >
+      {/* display the dynamic item information and description */}
       <div className='drawer-header'>
         <div>{selectedItem.name}</div>
         <div className='drawer-description'>{selectedItem.description}</div>
       </div>
+      {/* create a form that holds the inputs for updating the item data */}
       <Box sx={{ input: { color: 'white' }, marginTop: 2, marginLeft: 2, marginRight: 2 }}>
         <div> Item Name</div>
         <TextField sx={{ width: '100%' }} id='standard-basic' variant='standard' />
@@ -65,6 +70,19 @@ export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
           <div>Viewed</div>
         </Box>
       </Box>
+      {/* create a button to close the drawer from the inside */}
+      <Button
+        style={{
+          boxShadow: 'none',
+          borderRadius: '0px',
+          position: 'absolute',
+          bottom: 36,
+          width: '100%',
+          background: green[500],
+        }}
+        variant='contained'>
+        Save
+      </Button>
       <Button
         onClick={() => setDrawerOpen(false)}
         style={{
