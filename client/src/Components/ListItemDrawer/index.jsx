609dcb746fc1abe2326198a59128bfd6ebce6e18
emily <eap6787@gmail.com>
2022-11-19 14:40:00 -0700

    add Item name, status, and item description inputs to drawer

--- client/src/Components/ListItemDrawer/index.jsx
+++ client/src/Components/ListItemDrawer/index.jsx
@@ -3,8 +3,12 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
 import React from 'react';
 import '../../assets/styles/index.css';
-import { Checkbox, Drawer, FormControlLabel, FormLabel, Input, Radio, RadioGroup } from '@mui/material';
+import { Checkbox, Drawer, FormControlLabel, FormLabel, Input, Radio, RadioGroup, FormControl } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { bgcolor, color, margin } from '@mui/system';
+import { ClassNames } from '@emotion/react';
 
 export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
   const defaultState = {
@@ -12,6 +16,12 @@ export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
     viewed: 'false',
     description: '',
     status: false,
+  };
+
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
   return (
@@ -26,7 +36,32 @@ export function ListItemDrawer({ drawerOpen, setDrawerOpen, selectedItem }) {
     >
       <div className='drawer-header'>
         <div>{selectedItem.name}</div>
+        <div className='drawer-description'>{selectedItem.description}</div>
       </div>
+      <Box sx={{ marginTop: 2, marginLeft: 2, marginRight: 2 }}>
+        <div> Item Name</div>
+        <TextField sx={{ width: '100%' }} id='standard-basic' variant='standard' />
+        <FormControl sx={{ marginBottom: 2 }}>
+          <FormLabel sx={{ color: 'white', width: '100%', marginTop: 2 }} id='demo-row-radio-buttons-group-label'>
+            Status
+          </FormLabel>
+          <RadioGroup row aria-labelledby='demo-row-radio-buttons-group-label' name='row-radio-buttons-group'>
+            <FormControlLabel value='New' control={<Radio />} label='New' />
+            <FormControlLabel value='Complete' control={<Radio />} label='Complete' />
+            <FormControlLabel value='In Progress' control={<Radio />} label='In Progress' />
+          </RadioGroup>
+        </FormControl>
+        <div>Item Description</div>
+        <TextField
+          sx={{ input: { color: 'white' }, width: '100%', marginTop: 0.5 }}
+          id='outlined-multiline-static'
+          multiline
+          rows={4}
+          value={value}
+          onChange={handleChange}
+        />
+      </Box>
+
       {/* <Input name='name' value='' onChange={() => {}} />
       <label>Description</label>
       <input name='somethingElse' value={defaultState.name} type='text' onChange={() => {}} />
